0e96ca342a115fc4948be621a9e5b02b91ee3331
chobyh <201411258@sangmyung.kr>
2017-11-16 19:26:22 +0900

    몬스터 쫓아오는 코드

---
+++ 몬스터 코드/Assets/02. Scripts/GameUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour {
+
+    public Text txtScore;
+    private int totScore = 0;
+
+	// Use this for initialization
+	void Start () {
+        //totScore = PlayerPrefs.GetInt("TOT_SCORE", 0);
+        //DispScore(0);
+	}
+	public void DispScore(int score)
+    {
+        totScore += score;
+        txtScore.text = "score <color=#ff0000>" + totScore.ToString() + "</color>";
+
+        PlayerPrefs.SetInt("TOT_SCORE", totScore);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
