2a8b70be26c6a8455f0f4ed8ad4cdca0e97e9fdf
chobyh <201411258@sangmyung.kr>
2017-12-13 20:39:36 +0900

    게임 + 인벤토리 병합 완료

---
+++ MemoryLane - 복사본/Assets/Scripts/WangGeun/ChaseCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseCharacter : MonoBehaviour {
+
+    public float speed;
+    float distance;
+
+    GameObject Target;
+	public Transform oriPosition;
+    public Transform comePosition;
+
+    // Use this for initialization
+    void Start () {
+        Target = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!Target.gameObject.GetComponent<CharactorController> ().isHide) {
+			this.transform.position = Vector2.MoveTowards (this.transform.position, Target.transform.position, speed * Time.deltaTime);
+		} else {
+			this.transform.position = Vector2.MoveTowards(this.transform.position, oriPosition.transform.position, speed * Time.deltaTime);
+		}
+    }
+}
---
+++ MemoryLane - 복사본/Assets/Scripts/WangGeun/DifferentSprite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifferentSprite : MonoBehaviour {
+
+    public Sprite offSprite;
+    public Sprite onSprite;
+
+    // Use this for initialization
+    void Start () {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = offSprite;
+	}
+	
+	// Update is called once per frame
+	public void changeSprite()
+    {
+        if(this.gameObject.GetComponent<SpriteRenderer>().sprite == offSprite)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = onSprite;
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = offSprite;
+        }
+    }
+}
