3b97dafd252db105c15dc19be25c8a83cfd230ed
Lee WhangGeun <dndwls1120@naver.com>
2017-12-14 21:28:14 +0900

    17.12.14 21:27 Update
    애니메이션 및 이벤트 재적용

--- MemoryLane/Assets/Scripts/WangGeun/DiaryManager.cs
+++ MemoryLane/Assets/Scripts/WangGeun/DiaryManager.cs
@@ -9,7 +9,7 @@ public class DiaryManager : MonoBehaviour {
     bool UpdateFamliy;
     bool UpdateNewspaper;
     bool UpdateAward;
-    bool UpdatePolice;
+    bool UpdatePicture;
 
     // Use this for initialization
     void Start () {
@@ -22,65 +22,57 @@ public class DiaryManager : MonoBehaviour {
 
     void UpdateDairy()
     {
-        if(eventFlow.index == 1)
+        if(eventFlow.index == 1) // 2번 일지
         {
             
         }
-        else if(characterController.isShout)
+        else if(characterController.isShout) // 3번 일지
         {
 
         }
-        else if (characterController.haveBabyDoll)
+        else if (characterController.isreadGuiltybook) // 4번 일지
         {
 
         }
-        else if (characterController.isreadFamilyPhoto)
+        else if (characterController.isreadCalender) // 5번 일지
+        {
+            
+        }
+        else if (characterController.isreadFamilyPhoto) // 6번 일지
         {
             UpdateFamliy = true;
         }
-        else if (characterController.isreadNewspaper)
+        else if (characterController.isreadNewspaper) // 7번 일지
         {
             UpdateNewspaper = true;
         }
-        else if (UpdateFamliy && UpdateNewspaper)
+        else if (UpdateFamliy && UpdateNewspaper) // 8번 일지
         {
 
         }
-        else if (characterController.isreadTV)
+        else if (characterController.isreadTV) // 9번 일지
         {
 
         }
-        else if (characterController.isreadGuiltybook)
+        else if (characterController.haveFathersLetter) // 10번 일지
         {
 
         }
-        else if (characterController.isreadCalender)
+        else if (characterController.haveDaughtersLetter) // 11번 일지
         {
 
         }
-        else if (characterController.isreadCalender)
-        {
-
-        }
-        else if (characterController.isreadCalender)
-        {
-
-        }
-        else if (characterController.havePolice)
-        {
-            UpdatePolice = true;
-        }
-        else if (characterController.haveAward)
+        else if (characterController.haveAward && characterController.havePolice) // 12번 일지
         {
             UpdateAward = true;
         }
-        else if (UpdatePolice && UpdateAward)
+        else if (characterController.havePicture) // 13번 일지
         {
-
+            UpdatePicture = true;
         }
-        else if (characterController.havePicture)
+        else if (UpdatePicture && UpdateAward) // 14번 일지
         {
-
+            
         }
     }
 }
