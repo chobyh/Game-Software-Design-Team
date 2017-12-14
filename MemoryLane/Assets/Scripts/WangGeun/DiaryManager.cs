a16775e54cc59d9fc0fac41649cf974efa1140f5
Lee WhangGeun <dndwls1120@naver.com>
2017-12-14 22:01:12 +0900

    17.12.14 22:00 Update
    업데이트

--- MemoryLane/Assets/Scripts/WangGeun/DiaryManager.cs
+++ MemoryLane/Assets/Scripts/WangGeun/DiaryManager.cs
@@ -11,68 +11,78 @@ public class DiaryManager : MonoBehaviour {
     bool UpdateAward;
     bool UpdatePicture;
 
-    // Use this for initialization
-    void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    AudioSource diary;
+    public AudioClip diarySound;
+
+    void Start()
+    {
+        diary = GetComponent<AudioSource>();
+        diary.clip = diarySound;
+    }
+
+    void Update()
+    {
         UpdateDairy();
     }
 
+    // Use this for initializations
     void UpdateDairy()
     {
         if(eventFlow.index == 1) // 2번 일지
         {
-            
+            diary.Play();
         }
         else if(characterController.isShout) // 3번 일지
         {
-
+            diary.Play();
         }
         else if (characterController.isreadGuiltybook) // 4번 일지
         {
-
+            diary.Play();
         }
         else if (characterController.isreadCalender) // 5번 일지
         {
-            
+            diary.Play();
         }
         else if (characterController.isreadFamilyPhoto) // 6번 일지
         {
             UpdateFamliy = true;
+            diary.Play();
         }
         else if (characterController.isreadNewspaper) // 7번 일지
         {
             UpdateNewspaper = true;
+            diary.Play();
         }
         else if (UpdateFamliy && UpdateNewspaper) // 8번 일지
         {
-
+            diary.Play();
         }
         else if (characterController.isreadTV) // 9번 일지
         {
-
+            diary.Play();
         }
         else if (characterController.haveFathersLetter) // 10번 일지
         {
-
+            diary.Play();
         }
         else if (characterController.haveDaughtersLetter) // 11번 일지
         {
-
+            diary.Play();
         }
         else if (characterController.haveAward && characterController.havePolice) // 12번 일지
         {
             UpdateAward = true;
+            diary.Play();
         }
         else if (characterController.havePicture) // 13번 일지
         {
             UpdatePicture = true;
+            diary.Play();
         }
         else if (UpdatePicture && UpdateAward) // 14번 일지
         {
-            
+            diary.Play();
         }
     }
 }
